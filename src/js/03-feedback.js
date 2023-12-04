652aee7c850fdde4bf8c9aea2791015bdf04549a
SvyrydovFedir <118449931+SvyrydovFedir@users.noreply.github.com>
2023-12-04 20:55:14 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,26 +4,19 @@ const STORAGE_KEY_NAME = 'feedback-form-state';
 
 const email = feedbackForm.email;
 const message = feedbackForm.message;
+let currentValues = {};
 
 feedbackForm.addEventListener('input', throttle(handlerInputForm, 500));
-feedbackForm.addEventListener('submit', throttle(handlerSubmitForm, 500));
+feedbackForm.addEventListener('submit', handlerSubmitForm);
+document.addEventListener('DOMContentLoaded', restoreFormData);
 
 function handlerInputForm(event) {
   event.preventDefault();
 
   if (event.target.tagName === 'INPUT' || event.target.tagName === 'TEXTAREA') {
-    const currentValues = {
-      email: email.value,
-      message: message.value,
-    };
-
+    currentValues.email = email.value;
+    currentValues.message = message.value;
     localStorage.setItem(STORAGE_KEY_NAME, JSON.stringify(currentValues));
-
-    const savedStorage = localStorage.getItem(STORAGE_KEY_NAME);
-    if (savedStorage) {
-      email.value = JSON.parse(savedStorage).email || '';
-      message.value = JSON.parse(savedStorage).message || '';
-    }
   }
 }
 
@@ -32,5 +25,16 @@ function handlerSubmitForm(event) {
 
   localStorage.removeItem(STORAGE_KEY_NAME);
   event.currentTarget.reset();
-  console.log(`email: ${email.value}; message ${message.value}`);
+  console.log({ email: currentValues.email, message: currentValues.message });
+}
+
+function restoreFormData() {
+  const savedStorage = JSON.parse(localStorage.getItem(STORAGE_KEY_NAME));
+
+  if (savedStorage) {
+    email.value = savedStorage.email || '';
+    message.value = savedStorage.message || '';
+  }
+
+  console.log(savedStorage);
 }
